374ab24cf1dc0498e33e0e4028f7aae4cca54e81
Mutesi <joanahmutesi38@gmail.com>
2025-02-24 17:06:35 +0300

    Week 2

---
+++ Week 2/conditions.js
@@ -0,0 +1,39 @@
+// if (condition){
+//     //This part of code runs if the condition is true
+// };
+
+let number = 6;
+if (number>0){
+    console.log(`${number} is a positive integer`)
+};
+
+//Raining
+let weather = "raining";
+if(weather ==="raining"){
+    console.log("Remember to bring an umbrella");
+};
+
+//Boolean
+let isRaining =true;
+if (isRaining){
+    console.log("Remember to always bring an umbrella");
+};
+
+let numberTwo = 6;
+if (number>0){
+    console.log(`${number} is a positive integer`)
+}
+else(
+    console.log("`${number} is a positive integer`")
+);
+
+let weatherNow = "sunny";
+if(weatherNow === "rainy"){
+    console.log("you need an umbrella")
+} else if(weatherNow ==="cloudy"){
+    console.log("you might need an umbrella")
+} else if(weatherNow==="sunny"){
+    console.log("wear light clothes")
+} else{
+    console.log("No need for an umbrella")
+}
